1fb15e586ee74e76cfceb2f14bd24af0856fb78a
HankMallard <howardl01@live.ca>
2016-11-16 14:32:43 -0500

    Added Search Bar feature

--- src/index.js
+++ src/index.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyAeZbQTi7hqlXcaVB2GXS_PXyZ0EuVl-48';
 
+
+
 // Create a component should produce some HTML
-const App = () => {
-  return <div> Hello! </div>
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = { videos:[] };
+
+    YTSearch({key:API_KEY, term:'Mass Effect'}, (videos) => {
+      this.setState({videos});
+
+    });
+  }
+  render(){
+    return(
+      <div>
+       <SearchBar />
+       <VideoList videos={this.state.videos} />
+      </div>
+     );
+  }
 }
 
 // Take this component and put it inside the DOM
